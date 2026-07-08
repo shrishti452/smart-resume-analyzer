c119d6f9acbb5f39138bd9b1ef08abad2750c935
Shrishti <shrishtiagarwal205@gmail.com>
2026-07-08 18:33:39 +0530

    chore: remove Vite starter template

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Smart Resume Analyzer</h1>
+      <p>Welcome to our project 🚀</p>
+    </div>
+  );
+}
+
+export default App;
